8a9d2bbd7fb3312b5c3d99bf2a9189540010af26
Orkhan Orujaliyev <o.orkhan@gmail.com>
2018-11-11 16:04:38 +0400

    pagination

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php  include 'includes/header.php'; ?>
 
   <?php
-
+    //query for pagination
     $sql = "SELECT u.name,u.surname,u.profile_img,p.title,p.body,p.id as pid FROM users u INNER JOIN posts p ON p.user_id = u.id ORDER BY p.id DESC";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
-    $posts = $stmt->fetchAll();
+    $total_num_of_records = $stmt->rowCount();
+    $total_num_of_records;
+    if (!isset($_GET["page"])) {
+      $page = 1;
+    } else {
+      $page = $_GET["page"];
+    }
+    $next_page = $page + 1;
+    $prev_page = $page - 1;
+    $records_per_page = 3;
+    $total_pages = ceil($total_num_of_records/$records_per_page);
+    $start_limit = ($page-1)*$records_per_page;
+    //query for display data
+    $sql = "SELECT u.name,u.surname,u.profile_img,p.title,p.body,p.id as pid FROM users u INNER JOIN posts p ON p.user_id = u.id ORDER BY p.id DESC LIMIT :start_limit, :records_per_page"; 
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':start_limit', $start_limit, PDO::PARAM_INT);
+    $stmt->bindParam(':records_per_page', $records_per_page, PDO::PARAM_INT);
+    $stmt->execute();
+    $posts = $stmt->fetchAll();    
+
    ?>
 
   <div class="section no-pad-bot" id="index-banner">
@@ -20,6 +39,19 @@
             <button type="button" name="total_like" id="total_like" class="btn orange">Likes: <?php $count_like =  count_content_like ($pdo, $post["pid"]); echo $count_like ?></button>
         </div>
       <?php endforeach; ?>
+      <div id="pagination" class="center-align">
+        <ul class = "pagination">
+          <li class = "<?= $page == 1 ? 'btn disabled': 'btn' ?>"><a class="white-text" href = "index.php?page=1">First page</a></li>
+          <li class="<?= $page == 1 ? 'hide': ''?>"><a href="index.php?page=<?= $prev_page ?>">
+          <i class = "material-icons">chevron_left</i></a></li>
+          <?php for ($i=1; $i <= $total_pages ; $i++) { ?>
+            <li class = "<?= $i == $page ? 'active' : 'waves-effect'?>"><a href = "index.php?page=<?= $i ?>"><?= $i ?></a></li>                     
+          <?php } ?>
+          <li class="<?= $page == $total_pages ? 'hide': ''?>"><a href="index.php?page=<?= $next_page ?>"><i class = "material-icons">chevron_right</i></a></li>
+          <li class = "<?= $page == $total_pages ? 'btn disabled': 'btn' ?>"><a class="white-text" href = "index.php?page=<?= $total_pages ?>">Last page</a></li>
+        </ul>
+      </div>
+      
     </div>
   </div>
 
--- pagination.php
+++ pagination.php
@@ -1,44 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Pagination</title>
+</head>
+<body>
 <?php
-
-require_once("includes/config.php");
-session_start();
-include "function.php";
-
-$record_per_page = 5;
-$page = '';
-$output = '';
- if (isset($_POST["page"])) {
-    $page = $_POST["page"];
-}else {
-    $page = 1;
-} 
-
-$start_from = ($page - 1) * $record_per_page;
-
-$sql = "SELECT u.name,u.surname,u.profile_img,p.title,p.body,p.id as pid FROM users u INNER JOIN posts p ON p.user_id = u.id ORDER BY p.id DESC LIMIT $start_from, $record_per_page";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$posts = $stmt->fetchAll();
-foreach ($posts as $post) {
-    $output .= '    
-    <div class="card-panel">
-        <img height="50" style="border-radius:50%;border:2px solid grey;" src="images/'.$post["profile_img"].' alt="">
-        <span class="black-text">'. $post["name"] . ' '.$post["surname"].' </span>
-        <h5 class="">'. $post["title"].'</h5>
-        <p>'. strlen($post["body"]) > 200 ? substr($post["body"],0,200) . "..." : $post["body"].' </p>
-        <a href="post.php?id='. $post["pid"].'" class="btn green">Read more..</a> 
-        <button type="button" name="total_like" id="total_like" class="btn orange">Likes:'. $count_like =  count_content_like ($pdo, $post["pid"]); $count_like.'</button>
-    </div>    
-    ';
+// connect to database
+$con = mysqli_connect('localhost','root','');
+mysqli_select_db($con, 'pagination');
+// define how many results you want per page
+$results_per_page = 10;
+// find out the number of results stored in database
+$sql='SELECT * FROM alphabet';
+$result = mysqli_query($con, $sql);
+$number_of_results = mysqli_num_rows($result);
+// determine number of total pages available
+$number_of_pages = ceil($number_of_results/$results_per_page);
+// determine which page number visitor is currently on
+if (!isset($_GET['page'])) {
+  $page = 1;
+} else {
+  $page = $_GET['page'];
 }
-
-$page_query = "SELECT * FROM posts ORDER BY id DESC";
-$stmt_page = $pdo->prepare($page_query);
-$stmt_page->execute();
-$total_records = $stmt_page->rowCount();
-$total_pages = ceil($total_records/$record_per_page);
-for ($i=1; $i < $total_pages; $i++) { 
-    $output .='<li class="waves-effect"><a href="#!">'.$i.'</a></li>';
+// determine the sql LIMIT starting number for the results on the displaying page
+$this_page_first_result = ($page-1)*$results_per_page;
+// retrieve selected results from database and display them on page
+$sql='SELECT * FROM alphabet LIMIT ' . $this_page_first_result . ',' .  $results_per_page;
+$result = mysqli_query($con, $sql);
+while($row = mysqli_fetch_array($result)) {
+  echo $row['id'] . ' ' . $row['alphabet']. '<br>';
 }
-echo $output;
+// display the links to the pages
+for ($page=1;$page<=$number_of_pages;$page++) {
+  echo '<a href="index.php?page=' . $page . '">' . $page . '</a> ';
+}
 ?>
+</body>
+</html>
